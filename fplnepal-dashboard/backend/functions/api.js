1a043455a00debd4d22875d42aaaeff26fc28c2a
jigsaw007 <joshianuj005@gmail.com>
2025-03-15 21:39:32 +0900

    fix

--- fplnepal-dashboard/backend/functions/api.js
+++ fplnepal-dashboard/backend/functions/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const serverless = require("serverless-http"); // Added for Netlify Functions
+const serverless = require("serverless-http"); // Added for Netlify Functions..
 require("dotenv").config();
 
 const app = express();
